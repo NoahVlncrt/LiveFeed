6dce37ea55f14b29db8b3e621c39cefb2b7fa504
NoahVlncrt <noah0000games@gmail.com>
2016-05-20 10:44:26 -0400

    almost finished up event ui

--- client/pages/event/event.js
+++ client/pages/event/event.js
@@ -1,3 +1,16 @@
+Template.eventPage.onRendered(function(){
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 10, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+})
+
 Template.eventPage.helpers({
   "eventData": function(){
     currentEvent = FlowRouter.getParam("id")
